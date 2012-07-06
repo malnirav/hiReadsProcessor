17d89ee4ade643301a2f319296aa5e698b45b645
Nirav Malani <malnirav@gmail.com>
2012-07-06 12:01:08 -0400

    changed tileSize to 10, stepSize to 5 and minScore & minIdentity back to 5 and 70 respectively

--- R/hiReadsProcessor.R
+++ R/hiReadsProcessor.R
@@ -1749,7 +1749,7 @@ summary.elegant <- function(sampleInfo,samplenames=NULL) {
 #' @param host name of the machine to run gfServer on. Default: localhost
 #' @param port a port number to host the gfServer with. Default is 5560.
 #' @param seqDir the path of nib/2bit files relative to the current working dir. Default is "/"
-#' @param gfServerOpts a character vector of options to be passed to gfServer command on top of server defaults. Default: c(repMatch=112312, stepSize=5, tileSize=8)
+#' @param gfServerOpts a character vector of options to be passed to gfServer command on top of server defaults. Default: c(repMatch=112312, stepSize=5, tileSize=10)
 #' @param waitTime number of seconds to wait for gfServer to load. Default if 120. Increase this number for larger genomes, but for humans it should be suffice!
 #'
 #' @return system command status for executing gfServer command.
@@ -1759,7 +1759,7 @@ summary.elegant <- function(sampleInfo,samplenames=NULL) {
 #' @example 
 #' #startgfServer(port=5560,seqDir="/usr/local/blatSuite34/hg18.2bit")
 #' #stopgfServer(port=5560)
-startgfServer <- function(host="localhost", port=5560, seqDir=NULL, gfServerOpts=c(repMatch=112312, stepSize=5, tileSize=8), waitTime=120) {
+startgfServer <- function(host="localhost", port=5560, seqDir=NULL, gfServerOpts=c(repMatch=112312, stepSize=5, tileSize=10), waitTime=120) {
     if(is.null(seqDir)) {
         stop("Please define the path of nib/2bit files containing the indexed reference sequence(s)")
     }
@@ -1898,7 +1898,7 @@ splitSeqsToFiles <- function(x, totalFiles=4, suffix="tempy", filename="queryFil
 #' @param host name of the machine running gfServer. Default is 'localhost' and only used when standaloneBlat=FALSE.
 #' @param parallel use parallel backend to perform calculation with \code{\link{foreach}}. Defaults to TRUE. If no parallel backend is registered, then a serial version of foreach is ran using \code{\link{registerDoSEQ()}}.
 #' @param gzipResults gzip the output files? Default is TRUE.
-#' @param blatParameters a character vector of options to be passed to gfClient/BLAT command except for output file format (psl) and nohead. Default: c(minIdentity=70, minScore=5, stepSize=5, tileSize=8, repMatch=112312, dots=50, q="dna", t="dna"). Be sure to only pass parameters accepted by either BLAT or gfClient. For example, if repMatch or stepSize parameters are specified when using gfClient, then the function will simply ignore them! The defaults are configured to align a 17bp sequence with 70% identity.
+#' @param blatParameters a character vector of options to be passed to gfClient/BLAT command except for output file format (psl) and nohead. Default: c(minIdentity=70, minScore=5, stepSize=5, tileSize=10, repMatch=112312, dots=50, q="dna", t="dna"). Be sure to only pass parameters accepted by either BLAT or gfClient. For example, if repMatch or stepSize parameters are specified when using gfClient, then the function will simply ignore them! The defaults are configured to align a 19bp sequence with 70% identity.
 #'
 #' @return a character vector of psl filenames.
 #'
@@ -1907,7 +1907,7 @@ splitSeqsToFiles <- function(x, totalFiles=4, suffix="tempy", filename="queryFil
 #' @examples
 #' #blatSeqs(dnaSeqs,subjectSeqs,blatParameters=c(minIdentity=90, minScore=10, tileSize=10, dots=10, q="dna", t="dna"))
 #' #blatSeqs(dnaSeqs,"/usr/local/blatSuite34/hg18.2bit",standaloneBlat=FALSE)
-blatSeqs <- function(query=NULL, subject=NULL, standaloneBlat=TRUE, port=NULL, host="localhost", parallel=TRUE, gzipResults=TRUE, blatParameters=c(minIdentity=70, minScore=5, stepSize=5, tileSize=8, repMatch=112312, dots=50, q="dna", t="dna")) {
+blatSeqs <- function(query=NULL, subject=NULL, standaloneBlat=TRUE, port=NULL, host="localhost", parallel=TRUE, gzipResults=TRUE, blatParameters=c(minIdentity=70, minScore=5, stepSize=5, tileSize=10, repMatch=112312, dots=50, q="dna", t="dna")) {
 
     ## get all BLAT options from the system for comparison to blatParameters later
     suppressWarnings(blatOpts <- unique(sub("\\s+-(\\S+)=\\S+.+", "\\1", grep("\\s+-.+=", system("blat",intern=TRUE), value=TRUE))))
